5047b2cc68ecd466f9d71176331e85bac8296338
Stanley Chen <dannyshien@gmail.com>
2018-11-13 13:42:15 -0500

    copy over files from schema and seed

---
+++ seed.sql
@@ -0,0 +1,17 @@
+insert into todos 
+    (name, completed)
+values
+    ('walk the cat', false),
+    ('repair the hyperdrive', false),
+    ('return to Dagobah', false),
+    ('play cards against humanity with yoda', false),
+    ('return david bowie''s texts', false);
+
+insert into users
+    (name, username, pwhash)
+values
+    ('aylin', 'aylin', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK'),
+    ('beth', 'beth', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK'),
+    ('chris', 'chris', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK'),
+    ('bruce', 'bruce', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK'),
+    ('alf', 'alf', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK');
